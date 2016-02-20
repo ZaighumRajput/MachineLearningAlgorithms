c84be6a4ecaf41ec48ce2e9cc24844fc8beb6047
Zaighum Rajput <zaighum47@gmail.com>
2016-02-20 01:15:37 -0500

    Perceptron Class

--- MachineLearningAlgorithms/MachineLearningAlgorithms/Perceptron.py
+++ MachineLearningAlgorithms/MachineLearningAlgorithms/Perceptron.py
@@ -37,5 +37,23 @@ class Perceptron(object):
             self : object
         """
         self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_= []
 
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, Y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update !=0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        retun np.where(self.net_input(X) >= 0.0, 1, -1)
 
